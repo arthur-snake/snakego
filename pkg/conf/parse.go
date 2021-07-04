e49c93ef2294376e3ada25d6a044b63a896b783b
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2021-07-04 22:47:20 +0300

    Refactor config

---
+++ pkg/conf/parse.go
@@ -0,0 +1,12 @@
+package conf
+
+import "github.com/caarlos0/env/v6"
+
+func ParseEnv() (*App, error) {
+	cfg := App{}
+	err := env.Parse(&cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
